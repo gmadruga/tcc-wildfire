7799ac069ca7e976489c5acbe0f34c7082805a76
Gabriel Madruga <gabrielsantosmadruga@gmail.com>
2022-08-15 01:08:57 -0300

    finaliza versão inicial do projeto de tcc

--- api/main.py
+++ api/main.py
@@ -1,1 +1,37 @@
-from fastapi impot FastAPi
+from fastapi import FastAPI
+from pydantic import BaseModel
+import redis
+from datetime import datetime
+
+class Item(BaseModel):
+    device_id: int
+    temperature: float
+    humidity: float
+    lat:float
+    lon:float
+
+
+
+redis_hostname = "127.0.0.1"
+redis_port = "6379"
+time_encode = "%d/%m/%Y, %H:%M:%S"
+app = FastAPI()
+
+
+@app.post("/insert_data/")
+async def insert_data(item: Item):
+    stream = "temp_data"
+    time = datetime.now().strftime(time_encode)
+
+    fields = {
+        "device_id":item.device_id,
+        "temperature":item.temperature,
+        "humidity":item.humidity,
+        "lat":item.lat,
+        "lon":item.lon,
+        "time":time
+        }   
+    redis_at = redis.Redis(host=redis_hostname,port=redis_port)
+    redis_at.xadd(stream, fields)
+    return "SEND"
+
---
+++ api/test_request.py
@@ -0,0 +1,49 @@
+import requests
+import random
+import json
+import time 
+
+class SensorSimulate:
+    def __init__(self,device_id,init_temp,init_humidity,latitude,longitude) -> None:
+        self.device_id = device_id
+        self.temp = init_temp
+        self.humidity = init_humidity
+        self.latitude = latitude
+        self.longitude = longitude
+        pass
+
+    def run(self,frequency):
+        while True:
+            t_sum_or_sub = random.random()
+            h_sum_or_sub = random.random()
+            t_multiplier = random.randint(1,3)
+            h_multiplier = random.randint(1,3)
+            temp = random.random()
+            humi = random.random()
+
+            if t_sum_or_sub > 0.5:
+                self.temp = self.temp + temp*t_multiplier 
+            else:
+                self.temp = self.temp - temp*t_multiplier 
+
+            if h_sum_or_sub > 0.5:
+                self.humidity = self.humidity + humi*h_multiplier 
+            else:
+                self.humidity = self.humidity - humi*h_multiplier 
+
+            fields = {
+                "device_id":self.device_id,
+                "temperature":self.temp,
+                "humidity":self.humidity,
+                "lat":self.latitude,
+                "lon":self.longitude
+                }   
+            json_object = json.dumps(fields, indent = 4) 
+
+            r = requests.post('http://127.0.0.1:8000/insert_data/',data = json_object)
+            print("Message Sent:", str(json_object))
+            print(r.text)
+            time.sleep(frequency)
+
+
+#%%
